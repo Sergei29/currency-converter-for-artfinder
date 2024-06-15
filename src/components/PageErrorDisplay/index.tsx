46ceb8d3c4bb5450bc2a6b2db6afb003ead407e4
Serge Basangovs <sergejs.basangovs@gmail.com>
2024-06-15 21:23:18 +0300

    feat: err component placeholder, and component unit test

---
+++ src/components/PageErrorDisplay/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {
+  error: string
+}
+
+const PageErrorDisplay = ({ error }: Props): JSX.Element => {
+  return <div>PageErrorDisplay</div>
+}
+
+export default PageErrorDisplay
