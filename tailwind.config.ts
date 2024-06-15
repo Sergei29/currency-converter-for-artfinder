f7fb8129a201cb74643876b26be273407df7a6f0
Serge Basangovs <sergejs.basangovs@gmail.com>
2024-06-15 22:03:44 +0300

    chore: initialised chadcn lib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,42 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+const config = {
+  darkMode: ['class'],
+  content: ['./src/**/*.{ts,tsx}'],
+  prefix: '',
   theme: {
+    colors: {
+      marine: {
+        500: '#32415f',
+        600: '#2d3750',
+        700: '#232d46',
+      },
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      colors: {
-        marine: {
-          500: '#32415f',
-          600: '#2d3750',
-          700: '#232d46',
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config
+
 export default config
